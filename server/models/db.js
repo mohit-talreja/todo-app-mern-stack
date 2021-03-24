ad7ea883c5ee033271f3889a8b210b4dbfadeb65
mohit-talreja <74865221+mohit-talreja@users.noreply.github.com>
2021-03-24 23:35:47 +0530

    Delete server directory

--- server/models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/ToDoApp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then((res) => console.log('Connected to MongoDB.'))
-.catch((err) => console.log('Error in connecting to MongoDB!'))
